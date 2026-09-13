77e38b2f05c219e42823caa05cf4d9f5f88d70d2
DESKTOP-DS9UU98\R6 Siege <anzortorikashvili@gmail.com>
2026-09-13 06:35:53 +0300

    modifications to the 3D models of the 2D characters

--- src/components/SquishyToy.js
+++ src/components/SquishyToy.js
@@ -275,6 +275,34 @@ const MODEL_3D = {
     dentStrength: 2.3,
     dentFloor: 0.72,
   },
+  6: {
+    asset: require('../../assets/models/puffington_3d.glb'),
+    visual: 1.9,
+    dentSigma: 0.3,
+    dentStrength: 2.3,
+    dentFloor: 0.72,
+  },
+  7: {
+    asset: require('../../assets/models/noodle_3d.glb'),
+    visual: 1.9,
+    dentSigma: 0.3,
+    dentStrength: 2.3,
+    dentFloor: 0.72,
+  },
+  8: {
+    asset: require('../../assets/models/glimmer_3d.glb'),
+    visual: 1.9,
+    dentSigma: 0.3,
+    dentStrength: 2.3,
+    dentFloor: 0.72,
+  },
+  9: {
+    asset: require('../../assets/models/ember_3d.glb'),
+    visual: 1.9,
+    dentSigma: 0.3,
+    dentStrength: 2.3,
+    dentFloor: 0.72,
+  },
 };
 
 export const MODEL_3D_IDS = new Set(Object.keys(MODEL_3D));
